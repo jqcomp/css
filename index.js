db05aceff8b93072e70458d8dbcce6d1a1089831
Eldar Djafarov <djkojb@gmail.com>
2013-08-11 10:51:52 -0300

    2013-07-03 09:30:16 -0400  version:2.0.3

--- index.js
+++ index.js
@@ -139,10 +139,12 @@ jQuery.fn.extend({
 		return showHide( this );
 	},
 	toggle: function( state ) {
-		var bool = typeof state === "boolean";
+		if ( typeof state === "boolean" ) {
+			return state ? this.show() : this.hide();
+		}
 
 		return this.each(function() {
-			if ( bool ? state : isHidden( this ) ) {
+			if ( isHidden( this ) ) {
 				jQuery( this ).show();
 			} else {
 				jQuery( this ).hide();
@@ -173,6 +175,7 @@ jQuery.extend({
 		"fontWeight": true,
 		"lineHeight": true,
 		"opacity": true,
+		"order": true,
 		"orphans": true,
 		"widows": true,
 		"zIndex": true,
